e050617df6b2cbbaa4b574ef8ba6662573472ccf
Thomas Wade <gigatech55@gmail.com>
2019-07-09 02:14:08 +0930

    Get a working draft

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
 Vue.component('login-app', {
     // Paste in the HTML below
     template: `
-        <div class="ui middle aligned centre aligned grid">
-            <div class="column">
-                <div class="login-form">
-                    <div class="logo"></div>
-                    <br>
-                    <input type="text" class="ui input" placeholder="AUTHENTICATION NAME">
-                    <br>
-                    <input type="password" class="ui input" placeholder="PASSWORD">
-                    <br>
-                    <button type="button" class="ui inverted orange button">
-                        AUTHENTICATE <i class="long arrow alternate right icon" />
-                    </button>
-                </div>
-            </div>
+        <div class="the-cooler-container">
+            <img src="logo.png" alt="" class="logo">
+            <input v-model="user" type="text" placeholder="IDENTIFIER" />
+            <input v-model="pass" type="password" placeholder="ACCESS CODE" />
+            <button @click="login">AUTHORISE</button>
         </div>
-    `
+    `,
+    data: function() {
+        return {
+            user: "",
+            pass: "",
+            credentials: [
+                {
+                    user: "RLY-0016-3",
+                    pass: "SOMEPASSWORD",
+                    name: "Rolly",
+                    displayableRank: ""
+                },
+                {
+                    user: "61288-2001-LT",
+                    pass: "SOMEPASSWORD",
+                    name: "L. Talbot",
+                    displayableRank: "PO3"
+                }
+            ]
+        }
+    },
+    methods: {
+        login: function() {
+            // Get  an uppercase'd username
+            var userUpper = this.user.toUpperCase();
+
+            // Iterate over credentials
+            for (var credential of this.credentials) {
+                // Check username
+                if (userUpper == credential.user) {
+                    // Check password
+                    if (this.pass == credential.pass) {
+                        // Print out a welcome message
+                        alert(`Welcome ${credential.displayableRank} ${credential.name}`);
+
+                        // Clear the credential fields
+                        this.user = "";
+                        this.pass = "";
+
+                        return;
+                    } else {
+                        // Print out an error
+                        alert("Incorrect access code");
+
+                        return;
+                    }
+                }
+            }
+
+            // Print out an error
+            alert("Could not find user '" + this.user + "'");
+        }
+    }
 });
 
 new Vue({ el: '#app' });
